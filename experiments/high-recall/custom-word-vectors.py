2e552ead9c786cacaf433745572d603e140971f4
Milos Zivic <miloszivic99@gmail.com>
2023-07-13 16:41:01 +0200

    Issue #16: Custom word vectors combined with POS/NER

--- experiments/high-recall/custom-word-vectors.py
+++ experiments/high-recall/custom-word-vectors.py
@@ -1,3 +1,5 @@
+import time
+
 import numpy as np
 from gensim.models import Word2Vec, KeyedVectors
 from gensim.utils import tokenize
@@ -9,7 +11,6 @@ from utils import load_dataset, train_save_word2vec, vectorize, check_performanc
 WORD2VEC_TRAINED = True
 N_NEIGHBOURS = 100  # number of similar questions
 METRIC = "cosine"  # metric to be used in KNN, e.g. "euclidean", "cityblock", "cosine"
-SENTENCE_VECTOR_STRATEGY = "sum"  # strategy of transforming word vectors to sentence vector, e.g. "sum", "average"
 
 
 def custom_word_vectors() -> None:
@@ -19,39 +20,35 @@ def custom_word_vectors() -> None:
     :return:
         None
     """
+    start_time = time.time()
     df = load_dataset("../../data/insurance_qna_dataset.csv")
     questions = np.unique(df.iloc[:, 0].to_numpy())
     questions = [list(tokenize(question.lower())) for question in questions]
 
-    tfidf_vectorizer = TfidfVectorizer(token_pattern="(?u)\\b\\w+\\b")
-    tfidf_questions = [" ".join(question) for question in questions]
-    tfidf_vectorizer.fit_transform(tfidf_questions)
-
     if WORD2VEC_TRAINED:
-        model, wv = Word2Vec.load("word2vec.model"), KeyedVectors.load(
+        _, wv = Word2Vec.load("word2vec.model"), KeyedVectors.load(
             "word2vec.wordvectors", mmap="r"
         )
         print("Word2Vec model loaded")
     else:
-        model, wv = train_save_word2vec(questions)
+        _, wv = train_save_word2vec(questions)
         print("Word2Vec model trained and saved")
 
+    tfidf_vectorizer = TfidfVectorizer(token_pattern="(?u)\\b\\w+\\b")
+    tfidf_vectorizer.fit_transform([" ".join(question) for question in questions])
+
     vectorized_questions = np.asarray(
-        [
-            vectorize(wv, question, SENTENCE_VECTOR_STRATEGY, tfidf_vectorizer)
-            for question in questions
-        ]
+        [vectorize(wv, tfidf_vectorizer, question) for question in questions]
     )
+    print("Questions vectorized")
 
     knn = NearestNeighbors(n_neighbors=N_NEIGHBOURS, metric=METRIC).fit(
         vectorized_questions
     )
     print("KNN fitted")
 
-    score = check_performance(
-        wv, knn, questions, SENTENCE_VECTOR_STRATEGY, tfidf_vectorizer
-    )
-    print(f"Score: {score:.2f}")
+    score = check_performance(wv, knn, tfidf_vectorizer, questions)
+    print(f"Score: {score:.2f} | ETA: {time.time() - start_time:.2f}s")
 
 
 if __name__ == "__main__":
--- experiments/high-recall/utils.py
+++ experiments/high-recall/utils.py
@@ -1,11 +1,22 @@
 import json
+import re
 
 import numpy as np
 import pandas as pd
+import spacy
 from gensim.models import Word2Vec, KeyedVectors
 from gensim.utils import tokenize
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.neighbors import NearestNeighbors
+from spacy.tokenizer import Tokenizer
+
+nlp = spacy.load("en_core_web_sm")
+nlp.tokenizer = Tokenizer(nlp.vocab, token_match=re.compile(r"\S+").match)
+
+SENTENCE_VECTOR_STRATEGY = "sum"  # strategy of transforming word vectors to sentence vector, e.g. "sum", "average"
+SENTENCE_VECTOR_WEIGHT = "none"  # weight type applied to word vector, e.g. "idf", "pos", "ner", "pos+ner"
+POS = {"NOUN": 5.0, "PROPN": 6.0, "VERB": 2.0, "ADJ": 4.0}
+NER = {"MONEY": 6.0, "CARDINAL": 5.0, "DATE ": 4.0, "FAC ": 4.0}
 
 
 def load_dataset(path: str) -> pd.DataFrame:
@@ -32,9 +43,7 @@ def train_save_word2vec(corpus: list[list[str]]) -> tuple[Word2Vec, KeyedVectors
     :return:
         tuple of Word2Vec model and its word vectors
     """
-    model = Word2Vec(
-        sentences=corpus, vector_size=100, window=5, min_count=1, workers=4, epochs=50
-    )
+    model = Word2Vec(sentences=corpus, vector_size=100, window=5, min_count=1, workers=4, epochs=50)
     model.save("word2vec.model")
 
     word_vectors = model.wv
@@ -44,39 +53,63 @@ def train_save_word2vec(corpus: list[list[str]]) -> tuple[Word2Vec, KeyedVectors
 
 
 def vectorize(
-    wv: KeyedVectors,
-    document: list[str],
-    strategy: str,
-    tfidf_vectorizer: TfidfVectorizer,
+    wv: KeyedVectors, tfidf_vectorizer: TfidfVectorizer, document: list[str]
 ) -> np.ndarray:
     """
     Transforms documents to vectors
 
     :param wv: KeyedVectors
         vectors of all words from vocabulary
-    :param document:
-        input document from corpus
-    :param strategy: str
-        strategy of transforming word vectors
     :param tfidf_vectorizer: TfidfVectorizer
         TF-IDF vectorizer
+    :param document:
+        input document from corpus
     :return:
         vector representation of question
     """
-    token_positions = [tfidf_vectorizer.vocabulary_[token] for token in document]
-    idf = np.asarray(
-        [tfidf_vectorizer.idf_[token_position] for token_position in token_positions]
-    )
-    document = np.asarray([wv[token] for token in document])
-    document = idf[:, np.newaxis] * document
+    if SENTENCE_VECTOR_WEIGHT == "idf":
+        print("IDF")
+        idf = np.asarray(
+            [
+                tfidf_vectorizer.idf_[tfidf_vectorizer.vocabulary_[token]]
+                for token in document
+            ]
+        )
+        document = np.asarray([wv[token] for token in document])
+        document = idf[:, np.newaxis] * document
+    elif SENTENCE_VECTOR_WEIGHT == "pos":
+        print("POS")
+        doc = nlp(" ".join(document))
+        pos = np.asarray([POS.get(token.pos_, 1.0) for token in doc])
 
-    if strategy == "sum":
+        document = np.asarray([wv[token] for token in document])
+        document = pos[:, np.newaxis] * document
+    elif SENTENCE_VECTOR_WEIGHT == "ner":
+        print("NER")
+        doc = nlp(" ".join(document))
+        ner = np.asarray([NER.get(token.ent_type_, 1.0) for token in doc])
+
+        document = np.asarray([wv[token] for token in document])
+        document = ner[:, np.newaxis] * document
+    elif SENTENCE_VECTOR_WEIGHT == "pos+ner":
+        print("POS+NER")
+        doc = nlp(" ".join(document))
+        pos = np.asarray([POS.get(token.pos_, 1.0) for token in doc])
+        ner = np.asarray([NER.get(token.ent_type_, 1.0) for token in doc])
+        pos_ner = pos + ner
+
+        document = np.asarray([wv[token] for token in document])
+        document = pos_ner[:, np.newaxis] * document
+    else:
+        document = np.asarray([wv[token] for token in document])
+
+    if SENTENCE_VECTOR_STRATEGY == "sum":
         document = np.sum(document, axis=0)
-    elif strategy == "average":
+    elif SENTENCE_VECTOR_STRATEGY == "average":
         document = np.mean(document, axis=0)
     else:
         raise ValueError(
-            f"Strategy {strategy} is not supported. Try 'sum' or 'average'"
+            f"Strategy {SENTENCE_VECTOR_STRATEGY} is not supported. Try 'sum' or 'average'"
         )
 
     return document
@@ -85,9 +118,8 @@ def vectorize(
 def check_performance(
     wv: KeyedVectors,
     knn: NearestNeighbors,
-    questions: list,
-    strategy: str,
     tfidf_vectorizer: TfidfVectorizer,
+    corpus: list[list[str]],
 ) -> float:
     """
     Calculate performance of finding similar questions
@@ -96,12 +128,10 @@ def check_performance(
         vectors of all words from vocabulary
     :param knn: NearestNeighbors
         K-nearest neighbors
-    :param questions: list
-        input questions
-    :param strategy: str
-        strategy of transforming word vectors
     :param tfidf_vectorizer: TfidfVectorizer
         TF-IDF vectorizer
+    :param corpus: list
+        input corpus of documents
     :return:
         score (lesser is better)
     """
@@ -116,13 +146,12 @@ def check_performance(
     for i, tq in enumerate(test_questions):
         test_questions[i] = list(filter(lambda x: x in wv.index_to_key, tq))
     test_questions = np.asarray(
-        [vectorize(wv, tq, strategy, tfidf_vectorizer) for tq in test_questions]
+        [vectorize(wv, tfidf_vectorizer, tq) for tq in test_questions]
     )
     _, indices = knn.kneighbors(test_questions)
 
     original = [list(tokenize(o.lower())) for o in original]
-
-    indices_original = np.asarray([questions.index(o) for o in original])
+    indices_original = np.asarray([corpus.index(o) for o in original])
 
     rank = np.where(indices == indices_original[:, None])[1]
     penalization = (indices_original.shape[0] - rank.shape[0]) * 2 * knn.n_neighbors
